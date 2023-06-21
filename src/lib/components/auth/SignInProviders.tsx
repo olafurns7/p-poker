7ecdb9e7526fba9868f6f5386461bd016b99c191
olafurns7 <olafurns@icelandair.is>
2023-06-21 14:32:02 +0000

    feat(auth): add github authentication

--- src/lib/components/auth/SignInProviders.tsx
+++ src/lib/components/auth/SignInProviders.tsx
@@ -1,14 +1,22 @@
 import { Button, Grid, Text, useToast } from '@chakra-ui/react';
 import { useEffect } from 'react';
+import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 
-import { loginWithGoogle } from '~/lib/services/firebase/auth/login/google';
+import {
+  loginWithGoogle,
+  loginWithGithub,
+} from '~/lib/services/firebase/auth/login/google';
 
 const SignInProviders = () => {
   const toast = useToast();
 
   const handleLoginWithGoogle = async () => {
     await loginWithGoogle();
+  };
+
+  const handleLoginWithGithub = async () => {
+    await loginWithGithub();
   };
 
   useEffect(() => {
@@ -28,6 +36,13 @@ const SignInProviders = () => {
         _hover={{ bgGradient: 'linear(to-r, blue.100, cyan.100)' }}
       >
         Google
+      </Button>
+      <Button
+        leftIcon={<AiFillGithub />}
+        onClick={handleLoginWithGithub}
+        _hover={{ bgGradient: 'linear(to-r, blue.100, cyan.100)' }}
+      >
+        Github
       </Button>
     </Grid>
   );
--- src/lib/services/firebase/auth/login/google.ts
+++ src/lib/services/firebase/auth/login/google.ts
@@ -1,10 +1,20 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  signInWithPopup,
+} from 'firebase/auth';
 
 import { auth } from '~/lib/services/firebase/auth/common';
 import { showErrorToast } from '~/lib/services/firebase/utils';
 
 const googleProvider = new GoogleAuthProvider();
 
+const githubProvider = new GithubAuthProvider();
+
 export const loginWithGoogle = async () => {
   signInWithPopup(auth, googleProvider).catch((err) => showErrorToast(err));
+};
+
+export const loginWithGithub = async () => {
+  signInWithPopup(auth, githubProvider).catch((err) => showErrorToast(err));
 };
